d3f4398ffa036375ed8f4d010854eac34e028f98
brunogleite <brunoleite6o@gmail.com>
2021-04-16 23:07:00 +0100

    web travel styling

--- algarve/app.js
+++
@@ -1,5 +0,0 @@
-const translate = document.querySelector(".translate");
-
-translate.addEventListener("scroll", (e) =>{
-    console.log(e)
-})
--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log(document)
+/*const translate = document.querySelectorAll(".translate");
+const big_title = document.querySelectorAll(".main-title");
+const header = document.querySelectorAll(".lisbon-main")
+
+let header_height = header.offsetHeight;
+console.log(header_height)
+
+window.addEventListener("scroll", () =>{
+    let scroll = window.pageYOffset;
+    
+    translate.forEach(element => {
+        let speed = element.dataset.speed;
+        element.style.transform = `translateY(${scroll * speed}px)`
+    })
+
+
+})*/
+
+
+//when we mouseover the region it shows another page
+
+
+//when we click on algarve , adicionamos uma classlist de active e aparece a background do algarve e as informaceos
+const algarve = document.querySelector("#PT-08");
+const secondBg = document.querySelector(".second");
+const regions = document.querySelector(".region-number");
+const footerDetails = document.querySelector(".details");
+
+
+
+
+algarve.addEventListener("click", ()=>{
+    algarve.style.fill = "rgba(0, 0, 0, 0.9)"
+    footerDetails.classList.add(".show");
+    secondBg.classList.add("active") // adicionamos a classe responsavel por mudar o background
+    regions.innerHTML = "31"
+})
